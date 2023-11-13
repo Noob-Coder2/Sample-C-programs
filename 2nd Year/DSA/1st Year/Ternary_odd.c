0a72cf1632b706f99e24a3b1561f1c0ca671bfe2
Noob-Coder2 <133579536+Noob-Coder2@users.noreply.github.com>
2023-11-13 14:06:43 +0530

    Delete 2nd Year/DSA directory

--- 2nd Year/DSA/1st Year/Ternary_odd.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    int n;
-    printf("Enter the Number :");
-    scanf("%d",&n);
-    n%4==0?n%100==0?n%400==0?printf("Leap year"):printf("Not Leap Year"):printf("Leap Year"):printf("Not Leap Year"); 
-}
